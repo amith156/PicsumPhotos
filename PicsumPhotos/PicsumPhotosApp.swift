85e9851c47a7d840c2bf04f88fba788e2d8e0f34
Amith's MacBook <aknkum@essex.ac.uk>
2021-07-26 21:10:21 +0100

    Initial Commit

---
+++ PicsumPhotos/PicsumPhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicsumPhotosApp.swift
+//  PicsumPhotos
+//
+//  Created by Amith Narayan on 26/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct PicsumPhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
